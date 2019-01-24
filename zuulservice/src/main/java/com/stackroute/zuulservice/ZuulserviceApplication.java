e34d15def09bb20c92bb81697d992017376a835a
pratypr1 <pratypr1@in.ibm>
2019-01-24 10:15:09 +0530

    added  zuulservice folder

---
+++ zuulservice/src/main/java/com/stackroute/zuulservice/ZuulserviceApplication.java
@@ -0,0 +1,43 @@
+package com.stackroute.zuulservice;
+
+import com.stackroute.zuulservice.filter.ErrorFilter;
+import com.stackroute.zuulservice.filter.PostFilter;
+import com.stackroute.zuulservice.filter.PreFilter;
+import com.stackroute.zuulservice.filter.RouteFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulserviceApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(ZuulserviceApplication.class, args);
+	}
+	@Bean
+	public PreFilter preFilter()
+	{
+		return new PreFilter();
+	}
+	@Bean
+	public PostFilter postFilter()
+	{
+		return new PostFilter();
+	}
+	@Bean
+	public ErrorFilter errorFilter()
+	{
+		return new ErrorFilter();
+	}
+	@Bean
+	public RouteFilter routeFilter()
+	{
+		return new RouteFilter();
+	}
+}
+
+
+
